c31e471308a8a011f3d73f021b3f4495fdabef4c
miguelCassimiro99 <miguel.cassimiro99@gmail.com>
2021-08-06 20:47:56 -0300

    using an external api

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,5 +1,6 @@
 const moment = require('moment')
 const conexao = require("../infraestrutura/conexao")
+const axios = require('axios')
 
 class Atendimento {
     adiciona(atendimento, res) {
@@ -64,11 +65,16 @@ class Atendimento {
     buscaPorId(id, res) {
         const sql = `SELECT * FROM atendimentos WHERE id=${id}`
 
-        conexao.query(sql, (erro, resultados) => {
+        conexao.query(sql, async(erro, resultados) => {
+
             const atendimento = resultados[0]
+
+            const cpf = atendimento.cliente
             if (erro) {
                 res.status(400).json(erro)
             } else {
+                const { data } = await axios.get(`http://localhost:8082/${cpf}`)
+                atendimento.cliente = data
                 res.status(200).json(atendimento)
             }
         })
